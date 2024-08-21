ac71ecd8988dfbf0bb1fa96ad7bc505de0e427fa
Yi-Ta <f8162426@hotmail.com>
2024-08-20 20:27:09 -0700

    add index js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+let programName = prompt(
+  "Please enter your favorite programming language: ",
+  "JavaScript"
+);
+
+document.getElementById("programLanguage").textContent = programName;
