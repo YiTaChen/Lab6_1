931a77f4306916faa7b019a5de1ecd1724f55604
Yi-Ta <f8162426@hotmail.com>
2024-08-21 19:21:08 -0700

    update contact and project js

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,25 @@
-let contact_msg = document.getElementById("contact_message").textContent;
-console.log(contact_msg);
+function checkData() {
+  let name = document.getElementById("contact_name").value;
+  let email = document.getElementById("contact_email").value;
+  let phone = document.getElementById("contact_phone").value;
+  let contact_msg = document.getElementById("contact_message").value;
+
+  if (name.length < 5) {
+    alert("Name must be at least 5 characters.");
+  }
+  if (
+    String(email).indexOf("@") == -1 ||
+    email.length < 5 ||
+    String(email).indexOf(".") == -1
+  ) {
+    alert("Email must be format of xxx@xxx.xxx");
+  }
+
+  if (isNaN(Number(phone))) {
+    alert("Phone number must use only numbebrs.");
+  }
+
+  if (contact_msg.length < 5) {
+    alert("Message must be at least 5 characters.");
+  }
+}
--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,14 @@
-let project1 = prompt("Please enter 1st project name: ");
-let project2 = prompt("Please enter 1st project name: ");
-let project3 = prompt("Please enter 1st project name: ");
-let project4 = prompt("Please enter 1st project name: ");
-let project5 = prompt("Please enter 1st project name: ");
+let t1 = document.getElementById("1st").textContent;
+let t2 = document.getElementById("2nd").textContent;
+let t3 = document.getElementById("3rd").textContent;
+let t4 = document.getElementById("4th").textContent;
+let t5 = document.getElementById("5th").textContent;
+
+let project1 = prompt("Please enter 1st project name: ", t1);
+let project2 = prompt("Please enter 2st project name: ", t2);
+let project3 = prompt("Please enter 3st project name: ", t3);
+let project4 = prompt("Please enter 4st project name: ", t4);
+let project5 = prompt("Please enter 5st project name: ", t5);
 
 document.getElementById("1st").textContent = project1;
 document.getElementById("2nd").textContent = project2;
